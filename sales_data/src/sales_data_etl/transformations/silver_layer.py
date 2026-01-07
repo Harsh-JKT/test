c915613dcce6a6c1714f48981333d84a44f529c4
Harsh-JKT <Harsh-JKT>
2026-01-07 06:13:05 +0000

    column renaming done

--- sales_data/src/sales_data_etl/transformations/silver_layer.py
+++ sales_data/src/sales_data_etl/transformations/silver_layer.py
@@ -8,9 +8,9 @@ from utilities.utils import auto_cdc_silver_table
 auto_cdc_silver_table(
     target_table=TABLES["clean_sales_data"],
     source_table=TABLES["raw_sales_data"],
-    keys=["billing_document","material","billing_type","batch","quantity"],
-    sequence_by="last_updated_datetime",
-    except_column_list=["_rescued_data","ppd","qtd","discount","acer","ladder_discount","add_on","tcs","round_off","10_ka_dam","fraight","claim_discount","a0bill_date"],
+    keys=["invoice_number","sku","invoice_type","batch","volume"],
+    sequence_by="__timestamp",
+    # except_column_list=["_rescued_data","ppd","qtd","discount","acer","ladder_discount","add_on","tcs","round_off","10_ka_dam","fraight","claim_discount","a0bill_date"],
     ignore_null_updates=False
 )
 
@@ -21,7 +21,7 @@ auto_cdc_silver_table(
 )
 def preprocess_sales_target():
     df = spark.read.table(TABLES["clean_sales_data"])
-    df = df.withColumnsRenamed(sales_data_column_rename)
+    # df = df.withColumnsRenamed(sales_data_column_rename)
     df = df.withColumn("invoice_date", py_func.to_date("invoice_date", "yyyyMMdd"))\
         .withColumn("period",py_func.date_format(py_func.trunc("invoice_date", "MM"), "yyyy-MM"))\
         .withColumn("new_rep_month", py_func.when((py_func.month("invoice_date") == 4) & (py_func.year("invoice_date") == 2024), py_func.date_format(py_func.date_sub("invoice_date", -1), "yyyy-MM")).otherwise(py_func.date_format(py_func.date_sub("invoice_date", -2), "yyyy-MM")))
